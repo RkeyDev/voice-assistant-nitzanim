5e41a7103e31c05c55a73d72568231e07c50f0be
tal680 <talmaromgross@gmail.com>
2025-03-28 16:48:39 +0300

    added sent whatsapp

---
+++ send_whatsapp.py
@@ -0,0 +1,35 @@
+import listen
+import components.speak as speak
+import webbrowser
+import pyautogui
+import time
+
+
+def send_whatapp() -> None:
+    # asking for keys to press
+    print("What is the message ?")
+    speak.speak("What is the message ?")
+    text: str = listen.listen()  # listening what key to press
+
+    # exit key press
+    if text == "over" or text == "finish" or text == "exit" or text == "stop":
+        return
+
+    phone_number: str = "+972 50-645-4778"
+
+    try:
+        webbrowser.open(f"https://web.whatsapp.com/send?phone={phone_number}&text={text}")
+        time.sleep(5)
+        pyautogui.press("enter")
+
+        # if we want to close the tab uncomment this
+        # time.sleep(3)
+        # pyautogui.hotkey("ctrl", "w")
+
+    except Exception as e:
+        print(e)
+        return None
+
+
+if __name__ == '__main__':
+    raise RuntimeError("This script is not meant to be run directly.")
